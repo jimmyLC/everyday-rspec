28924110d1265d1c5593f6f15fe08315db7c02b2
Jimmy <acsd68500@gmail.com>
2014-12-21 17:10:38 +0800

    refactor of factorygirl

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Contact, :type => :model do
+
+  it "has a valid factory" do
+    expect(build(:contact)).to be_valid
+  end
+
   it "is vaild with a first name, lastname and email" do
-    contact = Contact.new(
+    contact = build( :contact,
       firstname: "Aaron",
       lastname: "Summer",
       email: "tester@example.com")
@@ -10,39 +15,43 @@ RSpec.describe Contact, :type => :model do
   end
 
   it "invalid without a firstname" do
-    contact = Contact.new(firstname: nil)
+    contact = build(:contact, firstname: nil)
     contact.valid?
     expect(contact.errors[:firstname]).to include("can't be blank")
   end
 
   it "invalid without a lastname" do
-    contact = Contact.new(lastname: nil)
+    contact = build(:contact, lastname: nil)
     contact.valid?
     expect(contact.errors[:lastname]).to include("can't be blank")
   end
 
   it "invalid without a email address" do
-    contact = Contact.new(email: nil)
+    contact = build(:contact, email: nil)
     contact.valid?
     expect(contact.errors[:email]).to include("can't be blank")
   end
+
   it "invalid without a duplicate email address" do
-    Contact.create(
-                  firstname: "jimmy",
-                  lastname: "Lin",
-                  email: "aaa@aaa.com")
-    contact = Contact.create(
-                  firstname: "jim",
-                  lastname: "L",
-                  email: "aaa@aaa.com")
+    create(:contact, email: "aaa@aaa.com")
+    contact = build(:contact, email: "aaa@aaa.com")
+
     contact.valid?
     expect(contact.errors[:email]).to include("has already been taken")
   end
+
   it "returns a contact's full name as a string" do
-    contact = Contact.create(
+    contact = build( :contact,
                   firstname: "jimmy",
-                  lastname: "Lin",
-                  email: "aaa@aaa.com")
+                  lastname: "Lin")
     expect(contact.name).to eq("jimmy Lin")
   end
+
+
 end
+
+
+
+
+
+
