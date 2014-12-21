946e470aebd2a23d8a05c0c5367b5538c1190549
Jimmy <acsd68500@gmail.com>
2014-12-21 18:33:52 +0800

    using faker

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -2,4 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Phone, :type => :model do
 
+  it "doesn't allow duplicate phone numbers per contact" do
+    contact = create(:contact)
+    create(:home_phone,
+      contact: contact,
+      phone: "785-555-1234")
+    mobile_phone = build(:mobile_phone,
+      contact: contact,
+      phone: "785-555-1234")
+    mobile_phone.valid?
+    expect(mobile_phone.errors[:phone]).to include("has already been taken")
+  end
+
+  it "allow two contacts to share a phone number" do
+    create(:home_phone,
+      phone: "785-555-1234")
+    expect(build(:home_phone,
+      phone: "785-555-1234")).to be_valid
+  end
 end
